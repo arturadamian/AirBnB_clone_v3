99ef4fa4daeb12e22a7d7eb95164f3a1cb1658ff
Donnerschlag <andrew.suh.94@gmail.com>
2018-08-20 14:00:13 -0700

    Commented on functions

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Creates a blueprint for flask
+"""
 from models import storage, Blueprint
 from api.v1.views import app_views
 from flask import Flask
@@ -6,14 +9,21 @@ from flask import Flask
 
 app = Flask(__name__)
 app_views = Blueprint('app_views', __name__)
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def teardown_appcontext(self):
+    """
+    Created teardown for closing storage
+    """
     storage.close()
 
 
 if __name__ == "__main__":
+    """
+    sets the port and host name depending on input
+    """
     if HBNB_API_HOST is not None:
         hosts = HBNB_API_HOST
     else:
