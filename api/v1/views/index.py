da9cc7f3712b66d2d127cf014de57e2dc9dcdd98
yook00627 <yook00627@gmail.com>
2018-08-20 15:08:24 -0700

    added stats for the index to have number of objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,33 @@
 """creates a route"""
 from api.v1.views import app_views
 from flask import jsonify
+import models
 
 
 @app_views.route("/status")
 def status():
     """return status in json"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def stats():
+    """retrieves the number of each objects by type:"""
+    stat_dict = {"amenities": 0,
+                 "cities": 0,
+                 "places": 0,
+                 "reviews": 0,
+                 "states": 0,
+                 "users": 0}
+    name_dict = {"Amenity": "amenities",
+                  "City": "cities",
+                  "Place": "places",
+                  "Review": "reviews",
+                  "State": "states",
+                  "User": "users"}
+    for obj in name_dict:
+        try:
+            stat_dict[name_dict[obj]] = models.storage.count(
+                models.classes(obj))
+        except Exception:
+            continue
+    return jsonify(stat_dict)
