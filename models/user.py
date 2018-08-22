de02d14182ed59e6c69ad5c82e968ccb4cb8a927
yook00627 <yook00627@gmail.com>
2018-08-22 11:01:13 -0700

    make the hash run properly

--- models/user.py
+++ models/user.py
@@ -16,7 +16,7 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
         email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
+        _password = Column("password", String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user",
@@ -25,22 +25,22 @@ class User(BaseModel, Base):
                                cascade="all, delete, delete-orphan")
     else:
         email = ""
-        password = ""
+        _password = ""
         first_name = ""
         last_name = ""
 
     @property
-    def pasd(self):
+    def password(self):
         """password getter
         Return:
             password that is set
         """
-        return self.password
+        return self._password
 
-    @pasd.setter
-    def pasd(self, pwd):
+    @password.setter
+    def password(self, pwd):
         """password setter
         Args:
             passwrod: input password
         """
-        self.password = md5(pwd.encode()).hexdigest()
+        self._password = md5(pwd.encode()).hexdigest()
