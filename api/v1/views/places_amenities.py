dd3b6363fb2bc32cd31ca0c2d41c49298ebb63ba
yook00627 <yook00627@gmail.com>
2018-08-22 11:36:31 -0700

    created place amenity that can linke amenities to place object and delte the connection and get the connection and show

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Create Place objects that handles all default RestFul API actions"""
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models import Place
+from models import Amenity
+
+
+@app_views.route("/places/<uuid:place_id>/amenities", methods=["GET"])
+def get_all_amenities_by_place(place_id):
+    """Retrieves the list of all Amenity objects of a Place
+    Args:
+        place_id: id of the place in uuid format
+    Return:
+        jsonified version of the amenity list
+    """
+    if storage.get("Place", place_id) is None:
+        abort(404)
+    amenity_list = []
+    for key, value in storage.all("Amenity").items():
+        if value.place_id == str(place_id):
+            amenity_list.append(value.to_dict())
+    return jsonify(amenity_list)
+
+
+@app_views.route("/places/<uuid:place_id>/amenities/<uuid:amenity_id>",
+                 methods=["DELETE"])
+def delete_place_amenity_object(place_id, amenity_id):
+    """Deletes a place_amenity object
+    Args:
+        place_id: id of the place in uuid format
+        amenity_id: id of the amenity in uuid format
+    Return:
+        jsonified version of empty dictionary with status code 200
+    """
+    try:
+        place = storage.get("Place", place_id)
+        amenity = storage.get("Amenity", amenity_id)
+        place_amenity = place.amenities
+        place_amenity.remove(amenity)
+        place.save()
+        return jsonify({}), 200
+    except Exception:
+        abort(404)
+
+
+@app_views.route("places/<uuid:place_id>/amenities/<uuid:amenity_id>",
+                 methods=["POST"])
+def post_place_amenity(place_id, amenity_id):
+    """Link a Amenity object to a Place
+    Args:
+        place_id: id of the place in uuid format
+        amenity_id: id of the amenity in uuid format
+    Returns:
+        jsonified version of amenity objecto
+    """
+    try:
+        place = storage.get("Place", place_id)
+        amenity = storage.get("Amenity", amenity_id)
+        place_amenity = place.amenities
+        if amenity in place_amenity:
+            return jsonify(amenity.to_dict()), 200
+        place_amenity.append(amenity)
+        place.save()
+        return jsonify(amenity.to_dict()), 201
+    except Exception:
+        abort(404)
