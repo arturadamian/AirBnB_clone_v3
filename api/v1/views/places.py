2361affe48c5119cc60f2cfaa383efafa61e1b5e
Donnerschlag <andrew.suh.94@gmail.com>
2018-08-23 12:01:14 -0700

    Tweaked to_dict to remove objects from final list

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -116,9 +116,8 @@ def get_places():
     all_states = json.get("states", [])
     all_cities = json.get("cities", [])
     if all_states == [] and all_cities == []:
-        for value in storage.all("Place").values():
-            place_list.append(value.to_dict())
-        return jsonify(place_list), 200
+        for indiv_place in storage.all("Place").values():
+            temp_place.add(indiv_place)
     json = request.get_json()
     if all_states != []:
         for indiv in all_states:
@@ -137,12 +136,20 @@ def get_places():
     if all_amenities != []:
         all_amen_obj = set()
         temp_copy = temp_place.copy()
+        print(temp_copy)
+        print(len(temp_copy))
         for indiv in all_amenities:
             all_amen_obj.add(storage.get("Amenity", indiv))
         for indiv_places in temp_copy:
-            for indiv_amen in indiv_places.amenities:
+            amenities = indiv_places.amenities
+            for indiv_amen in amenities:
+                print("temp_place_indiv: ", indiv_amen, "\n")
+                print("all_amen_obj: ", all_amen_obj, "\n")
                 if indiv_amen not in all_amen_obj:
                     temp_place.discard(indiv_places)
+                    print("temp place: ", temp_place, "\n")
+                    break
     for indiv in temp_place:
         place_list.append(indiv.to_dict())
+    print("place _LIST: ", place_list)
     return jsonify(place_list), 200
--- models/base_model.py
+++ models/base_model.py
@@ -81,6 +81,10 @@ class BaseModel:
         if "_password" in cp_dct:
             cp_dct["password"] = cp_dct["_password"]
             cp_dct.pop("_password", None)
+        if 'amenities' in cp_dct:
+            cp_dct.pop('amenities', None)
+        if 'reviews' in cp_dct:
+            cp_dct.pop('reviews', None)
         if getenv('HBNB_TYPE_STORAGE') == 'db':
             cp_dct.pop("password", None)
         return (cp_dct)
